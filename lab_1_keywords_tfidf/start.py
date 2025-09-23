75cba02ee5650223e53dada2b4c3ef698f8af489
julia <juliamixi@mail.ru>
2025-09-24 00:46:09 +0300

    fix ci errors

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -6,12 +6,12 @@ Frequency-driven keyword extraction starter
 from json import load
 
 from lab_1_keywords_tfidf.main import (
-    clean_and_tokenize,
-    remove_stop_words,
     calculate_frequencies,
-    get_top_n,
     calculate_tf,
-    calculate_tfidf
+    calculate_tfidf,
+    clean_and_tokenize,
+    get_top_n,
+    remove_stop_words,
 )
 
 def main() -> None:
@@ -26,14 +26,31 @@ def main() -> None:
         idf = load(file)
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
         corpus_freqs = load(file)
-    result = None
+    
     tokens = clean_and_tokenize(target_text)
-    filteted_tokens = remove_stop_words(tokens, stop_words)
-    frequencies = calculate_frequencies(filteted_tokens)
+    if tokens is None:
+        return
+    
+    filtered_tokens = remove_stop_words(tokens, stop_words)
+    if filtered_tokens is None:
+        return
+    
+    frequencies = calculate_frequencies(filtered_tokens)
+    if frequencies is None:
+        return
+    
     tf = calculate_tf(frequencies)
+    if tf is None:
+        return
+    
     tfidf = calculate_tfidf(tf, idf)
-    top_tfidf = get_top_n(tfidf, 10)
-    result = top_tfidf
+    if tfidf is None:
+        return
+    
+    result = get_top_n(tfidf, 10)
+    if result is None:
+        return
+    
     print(result)
     assert result, "Keywords are not extracted"
 
