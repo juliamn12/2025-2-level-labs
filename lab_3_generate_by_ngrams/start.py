83ca730c913e012c37fefa9073e6eace5b9a8b87
julia <juliamixi@mail.ru>
2025-11-16 23:28:32 +0300

    fix start.py

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -19,21 +19,25 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
+
     text_processor = TextProcessor("_")
     encoded_text = text_processor.encode(text)
     if encoded_text is None:
         return
+    print(f"Encoded text:{encoded_text}")
     decoded_text = text_processor.decode(encoded_text)
-    print(decoded_text)
-    n_gram_size = 7
-    language_model = NGramLanguageModel(encoded_text, n_gram_size)
+    print(f"Decoded text: {decoded_text}")
+
+    language_model = NGramLanguageModel(encoded_text, n_gram_size=7)
     language_model.build()
     greedy_generator = GreedyTextGenerator(language_model, text_processor)
     greedy_text = greedy_generator.run(seq_len=51, prompt="Vernon")
     print(greedy_text)
+
     beam_search_generator = BeamSearchTextGenerator(language_model, text_processor, beam_width=3)
     beam_text = beam_search_generator.run(prompt="Vernon", seq_len=56)
     print(beam_text)
+
     result = beam_text
     assert result
 
